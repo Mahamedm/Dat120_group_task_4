dced776ba9cbae6c82f75a191f834d05e7dcff67
Mohammed Mahamuud M <moha714566@gmail.com>
2023-09-22 14:20:19 +0200

    task M

--- test.py
+++ test.py
@@ -1,29 +1,11 @@
 # Field to test the functions
 from list_operations import *
-
-# Task E
-
-# numbers = [1, 3, 7, 12, 20, 25]
-# differences = calculate_differences(numbers)
-# print("E: Differences between consecutive numbers:", differences)
-
-# # optional
-# data = [10, 15, 20, 25, 30, 35, 40, 45, 50]
-# e = 2  # Number of elements for averaging(current data point + the two next data points)
-# moving_avg = calculate_moving_averages(data, e)
-# derivative = calculate_derivative_of_averages(moving_avg)
-# print("E: Moving Averages:", moving_avg)
-# print("E: Numerical Derivative of Averages:", derivative)
-
-
-# # task F 
-# numbers = [2, 5, 0, 0, 0, 3, 6, 4, 0, 0, 5, 0, 12, 12, 12, 12, 7, 19]
-# print("F: Length of longest sequence of 0s:", longest_continuous_sequence_of_zeros(numbers))
-
-# optional 
-integers =  [2, 5, 0, 0, 0, 3, 6, 4, 0, 0, 5, 0, 12, 12, 12, 12, 7, 19]
-length, value = longest_sequence_of_same_value(integers)
-print(f"F: Length of longest sequence of {value}s:", length)
+# lister_for_del_1.py:
+temperaturer = [-5, 2, 6, 13, 9, 22, 28, 19, 24, 12, 5, 1, -3, -8, 2, 8, 15, 18, 21, 26, 21, 31, 15, 4, 1, -2]
+dogn_nedbor = [2, 5, 0, 0, 0, 3, 6, 4, 0, 0, 5, 0, 12, 12, 12, 12, 7, 19]
+temperaturer_tidspunkter = list()
+for index in range(len(temperaturer)):
+    temperaturer_tidspunkter.append(index)
 
 # Task J
 temperaturer, task_i_diffrence_print= [-5, 2, 6, 13, 9, 22, 28, 19, 24, 12, 5, 1, -3, -8, 2, 8, 15, 18, 21, 26, 21, 31, 15, 4, 1, -2],[]
@@ -37,11 +19,6 @@ for i in task_i_differences:
 print("i: Differences between consecutive numbers:", task_i_diffrence_print)
 
 # task K
-temperaturer = [-5, 2, 6, 13, 9, 22, 28, 19, 24, 12, 5, 1, -3, -8, 2, 8, 15, 18, 21, 26, 21, 31, 15, 4, 1, -2]
-temperaturer_tidspunkter = list()
-for index in range(len(temperaturer)):
-    temperaturer_tidspunkter.append(index)
-
 trend =calculate_trend(temperaturer_tidspunkter,temperaturer)[0]
 print("K: The trend of Temperaturer is:",trend)
 if (trend>0):
@@ -50,3 +27,7 @@ elif (trend<0):
     print("K: The trend is decreasing")
 else:
     print("There is no trend")
+
+# task M
+print(f"M: longest perion without precipitation is: {longest_continuous_sequence_of_zeros(dogn_nedbor)} days")
+
